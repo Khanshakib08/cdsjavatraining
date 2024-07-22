9d0e7ab83120ee7306fd0c9f0cdf016b3f741b17
Khanshakib08 <khanshakib1019@gmail.com>
2024-07-22 15:30:26 +0530

    Addition of two number with different datatype

---
+++ AddTwoNumberWithDifferentDatatype.java
@@ -0,0 +1,7 @@
+class AddTwoNumberWithDifferentDatatype{
+    public static void main(String[] args) {
+        int a=5;
+        float b=7.5f;
+        System.out.println("Sum of a & b is:"+((float)a+b));
+    }
+}
