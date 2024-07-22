bdd8391cee13d9601bf3c7101bd3bc5933eb156a
Khanshakib08 <khanshakib1019@gmail.com>
2024-07-22 16:31:19 +0530

    ternary operator

---
+++ TernaryOperator.java
@@ -0,0 +1,7 @@
+class TernaryOperator{
+    public static void main(String[] args) {
+        int age=21;
+       String adult = (age>=18)?"Eligible for voting":"Not aligible for voting";
+       System.out.println(adult);
+    }
+}
