be8a8dca6df9a5da41b20683850985ac645c23fe
Khanshakib08 <khanshakib1019@gmail.com>
2024-07-26 00:29:21 +0530

    Password Generator

---
+++ dsajava/src/Sample14.java
@@ -0,0 +1,10 @@
+
+public class Sample14 {
+        public static void main(String[] args) {
+             int n = 50;
+            while (n > 1) {
+                n /= 2;
+                System.out.println(n);
+        }
+    }
+}
